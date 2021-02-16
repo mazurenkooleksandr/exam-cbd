5288b539376d692e0d3b5972f36e116f5ac868b6
mazurenkooleksandr <kakchtoinfo@ukr.net>
2021-02-16 16:01:52 +0200

    tabs-done

---
+++ main.js/tab.js
@@ -0,0 +1,17 @@
+function openTab(evt, name) {
+    let i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("third-container__tab_tabcontent");
+    
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("third-container__tab_tablinks");
+    
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(name).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
